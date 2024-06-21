b940d293987df57a948f114c63989f604f19d20a
CMLiussss <24787744+cmliu@users.noreply.github.com>
2024-06-22 01:52:17 +0800

    Update _worker.src.js

--- _worker.src.js
+++ _worker.src.js
@@ -21,7 +21,7 @@ let addresses = [
 ];
 
 let sub = ''; 
-let subconverter = 'url.v1.mk';// clash订阅转换后端，目前使用肥羊的订阅转换功能。自带虚假节点信息防泄露
+let subconverter = 'subapi-loadbalancing.pages.dev';// clash订阅转换后端，目前使用CM的订阅转换功能。自带虚假节点信息防泄露
 let subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini.ini"; //订阅配置文件
 let RproxyIP = 'false';
 
